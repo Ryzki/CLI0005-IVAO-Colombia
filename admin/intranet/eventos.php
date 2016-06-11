a435fcc06594d8306d17a8fb86e0161e2150e8f4
Andres Esteban Zapata <Andres Esteban Zapata>
2016-06-11 16:02:40 -0500

    Mas

--- admin/intranet/eventos.php
+++ admin/intranet/eventos.php
@@ -20,7 +20,7 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <h3>IVAO AERO</h3>
-                                    <form role="form" action="./?page=addevento" method="post" enctype="multipart/form-data" id="MyUploadForm">
+                                    <form enctype="multipart/form-data"  action="./?page=addevento" method="post" >
                                         <div class="form-group">
                                             <label>Nombre Evento</label>
                                             <input class="form-control" name="nombre" />
@@ -43,172 +43,12 @@
                                         </div>
 										<div class="form-group">
                                             <label>Imagen Evento</label>
-                                           <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-
-<script type="text/javascript" src="js/jquery.form.min.js"></script>
-
-
-
-<script type="text/javascript">
-
-$(document).ready(function() { 
-
-	var options = { 
-
-			target: '#output',   // target element(s) to be updated with server response 
-
-			beforeSubmit: beforeSubmit,  // pre-submit callback 
-
-			success: afterSuccess,  // post-submit callback 
-
-			resetForm: true        // reset the form after successful submit 
-
-		}; 
-
-		
-
-	 $('#MyUploadForm').submit(function() { 
-
-			$(this).ajaxSubmit(options);  			
-
-			// always return false to prevent standard browser submit and page navigation 
-
-			return false; 
-
-		}); 
-
-}); 
-
-
-
-function afterSuccess()
-
-{
-
-	$('#submit-btn').show(); //hide submit button
-
-	$('#loading-img').hide(); //hide submit button
-
-
-
-}
-
-
-
-//function to check file size before uploading.
-
-function beforeSubmit(){
-
-    //check whether browser fully supports all File API
-
-   if (window.File && window.FileReader && window.FileList && window.Blob)
-
-	{
-
-		
-
-		if( !$('#imageInput').val()) //check empty input filed
-
-		{
-
-			$("#output").html("No image selected");
-
-			return false
-
-		}
-
-		
-
-		var fsize = $('#imageInput')[0].files[0].size; //get file size
-
-		var ftype = $('#imageInput')[0].files[0].type; // get file type
-
-		
-
-
-
-		//allow only valid image file types 
-
-		switch(ftype)
-
-        {
-
-            case 'image/png': case 'image/gif': case 'image/jpeg': case 'image/pjpeg':
-
-                break;
-
-            default:
-
-                $("#output").html("<b>"+ftype+"</b> Unsupported file type!");
-
-				return false
-
-        }
-
-		
-
-		//Allowed file size is less than 1 MB (1048576)
-
-		if(fsize>5048576) 
-
-		{
-
-			$("#output").html("<b>"+bytesToSize(fsize) +"</b> Too big Image file! <br />Please reduce the size of your photo using an image editor.");
-
-			return false
-
-		}
-
-				
-
-		$('#submit-btn').hide(); //hide submit button
-
-		$('#loading-img').show(); //hide submit button
-
-		$("#output").html("");  
-
-	}
-
-	else
-
-	{
-
-		//Output error to older browsers that do not support HTML5 File API
-
-		$("#output").html("Please upgrade your browser, because your current browser lacks some new features we need!");
-
-		return false;
-
-	}
-
-}
-
-
-
-//function to format bites bit.ly/19yoIPO
-
-function bytesToSize(bytes) {
-
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-
-   if (bytes == 0) return '0 Bytes';
-
-   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-
-   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-
-}
-
-
-
-</script>
-
-<input name="image_file" id="imageInput" type="file">
+                                           
+<input name="image_file"  type="file">
 
 <br>
 <hr>
 
-<img src="images/ajax-loader.gif" id="loading-img" style="display:none;" alt="Please Wait"/>
 										
                                         </div>
 										
