8ce5d0176db6d016dd113a357de3636c091927ad
Andres Esteban Zapata <Andres Esteban Zapata>
2016-06-12 15:03:41 -0500

    More

--- admin/intranet/index.php
+++ admin/intranet/index.php
@@ -82,7 +82,7 @@ include('./db_login.php');
 <head>
       <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>IVAO Colombia | Staff Admin</title>
+    <title>IVAO Colombia | Staff</title>
 	<!-- BOOTSTRAP STYLES-->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
      <!-- FONTAWESOME STYLES-->
@@ -189,8 +189,8 @@ font-size: 16px;"> Last access : <?php echo $last_visit_date; ?> &nbsp; <a href=
                     <i class="fa fa-envelope-o"></i>
                 </span>
                 <div class="text-box" >
-                    <p class="main-text">120 New</p>
-                    <p class="text-muted">Messages</p>
+                    <p class="main-text">5 VA's</p>
+                    <p class="text-muted">Aerolineas</p>
                 </div>
              </div>
 		     </div>
@@ -200,8 +200,8 @@ font-size: 16px;"> Last access : <?php echo $last_visit_date; ?> &nbsp; <a href=
                     <i class="fa fa-bars"></i>
                 </span>
                 <div class="text-box" >
-                    <p class="main-text">30 Tasks</p>
-                    <p class="text-muted">Remaining</p>
+                    <p class="main-text">+27</p>
+                    <p class="text-muted">Eventos</p>
                 </div>
              </div>
 		     </div>
@@ -211,8 +211,8 @@ font-size: 16px;"> Last access : <?php echo $last_visit_date; ?> &nbsp; <a href=
                     <i class="fa fa-bell-o"></i>
                 </span>
                 <div class="text-box" >
-                    <p class="main-text">240 New</p>
-                    <p class="text-muted">Notifications</p>
+                    <p class="main-text">+5</p>
+                    <p class="text-muted">Examenes</p>
                 </div>
              </div>
 		     </div>
@@ -222,8 +222,8 @@ font-size: 16px;"> Last access : <?php echo $last_visit_date; ?> &nbsp; <a href=
                     <i class="fa fa-rocket"></i>
                 </span>
                 <div class="text-box" >
-                    <p class="main-text">3 Orders</p>
-                    <p class="text-muted">Pending</p>
+                    <p class="main-text">+3</p>
+                    <p class="text-muted">Notams</p>
                 </div>
              </div>
 		     </div>
@@ -237,15 +237,395 @@ font-size: 16px;"> Last access : <?php echo $last_visit_date; ?> &nbsp; <a href=
                     <i class="fa fa-warning"></i>
                 </span>
                 <div class="text-box" >
-                    <p class="main-text">52 Important Issues to Fix </p>
-                    <p class="text-muted">Please fix these issues to work smooth</p>
-                    <p class="text-muted">Time Left: 30 mins</p>
+                    <p class="main-text">Staff's IVAO CO</p>
+                    <p class="text-muted">Información de Staff Activos en la división</p>
+                    <p class="text-muted">Time Left: 1 sec</p>
                     <hr />
                     <p class="text-muted">
-                          <span class="text-muted color-bottom-txt"><i class="fa fa-edit"></i>
-                               Lorem ipsum dolor sit amet, consectetur adipiscing elit gthn. 
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit gthn. 
-                               </span>
+  <?
+/**
+ * IVAO Traffic list
+ *
+ * @author Aki Kettunen www.akikettu.net
+ * @package defaultPackage
+ */
+/*
+BEGINNG OF CONFIGURATION
+*/
+
+/**
+ * 	EDIT by Chris Doehring (272909), 2012-12-04
+ *  Added check function for local airport country codes...
+ *
+ */
+ function checkCountryIcao($check) {
+	$countryicao = array('SK');
+	
+	foreach($countryicao as $id => $value) {
+		if(trim($value) == trim($check)) {
+			return true;
+		}
+	}
+	return false;
+ }
+
+
+
+// For easy translation..
+$lng['staffingb'] = 'Staff en linea';
+$lng['atcingb'] = 'ATC en linea - CO';
+$lng['noatcingb'] = 'No hay ATC en linea.';
+$lng['trafficingb'] = 'Trafico Salidas/Llegadas';
+$lng['notrafficingb'] = '_No hay trafico Salidas/Llegadas.';
+$lng['atcbingb'] = '<a href="http://www.ivao.aero/atcss/new.asp" target="_blank">Add a Booking</a>';
+$lng['noatcbingb'] = 'No se puede reservar.';
+$lng['totalonline'] = 'Hay %s ATC(s) y %s  piloto(s) conectado(s) en IVAO.';
+$lng['today'] = 'Hoy';
+$lng['tomorrow'] = 'Mañana';
+$weekdays = array(
+    1 => 'Lunes',
+    2 => 'Martes',
+    3 => 'Miercoles',
+    4 => 'Jueves',
+    5 => 'Viernes',
+    6 => 'Sabado',
+    0 => 'Domingo'
+);
+
+// Put here 2 first letter of airport ICAO codes
+#$countryicao = 'NW';
+
+// Put here country code of staff members
+$staffcountry = 'CO';
+
+$airports = array(
+'SKBO' => '', 
+'SKRG' => '', 
+'SKCL' => '', 
+'SKPE' => '', 
+'SKSP' => '', 
+'SKSM' => '', 
+'SKCG' => '', 
+'SKRH' => '', 
+'SKBG' => '', 
+'SKMD' => '', 
+'SKBQ' => '', 
+'SKBS' => '', 
+);
+
+$validcontrollers = array('DEL','GND','TWR','DEP','APP','CTR','FSS');
+
+$ctrlevel = array(
+   1 => 'OBS',
+   2 => 'AS1',
+   3 => 'AS2',
+   4 => 'AS3',
+   5 => 'ADC',
+   6 => 'ADP',
+   7 => 'ACC',
+   8 => '<span class="green">SEC</span>',
+   9 => '<span class="green">SAI</span>',
+  10 => '<span class="green">CAI</span>',
+  11 => '<span class="red">SUP</span>',
+  12 => '<span class="red">ADM</span>'
+);
+
+/*
+END OF CONFIGURATION
+*/
+
+//http://www.ivao.aero/whazzup/status.txt
+//http://dataservice.gatools.org/data/ivao.txt
+
+// DOWNLOAD ONLINE-LISTAUS
+//---------------------------------------------------------------------------------------------------------
+$filecontents = file_get_contents('http://api.ivao.aero/getdata/whazzup/whazzup.txt');
+$rows = explode("\n", $filecontents);
+
+$filepart = '';
+$pilots = array();
+$pilotcount = 0;
+$controllers = array();
+$staff = array();
+$controllercount = 0;
+$generaldata = array();
+
+foreach ($rows as $row) {
+    if (substr($row,0,1) == '!') {
+        $filepart = substr($row,1);
+    } else {
+        switch ($filepart) {
+            case 'CLIENTS':
+                $fields = explode(":", $row);
+                if ($fields[3] == 'ATC') {
+                    $controllercount++;
+                    if (in_array(substr($fields[0],-3), $validcontrollers) && checkCountryIcao(substr($fields[0],0,2))) { array_push($controllers, $fields); }
+                        if (substr($fields[0],0,3) == $staffcountry . '-') { array_push($staff, $fields); }
+                } else {
+                    $pilotcount++;
+                    if (checkCountryIcao(substr($fields[11],0,2)) OR checkCountryIcao(substr($fields[13],0,2))) {
+                        array_push($pilots, $fields);
+                    }
+                }
+                break;
+            case 'GENERAL':
+                list($key, $value) = explode('=', $row);
+                $generaldata[trim($key)] = trim($value);
+                break;
+        }
+    }
+}
+
+
+// DOWNLOAD BOOKING LIST
+//---------------------------------------------------------------------------------------------------------
+$filecontentsa = file_get_contents('http://www.ivao.aero/schedule/indd.asp');
+$rowsa = explode("\n", $filecontentsa);
+
+$filepart = '';
+$booking_pilots = array();
+$booking_controllers = array();
+
+foreach ($rowsa as $rowa) {
+    if (substr($rowa,0,1) == '!') {
+        $filepart = substr($rowa,1);
+    } else {
+        switch (trim($filepart)) {
+            case 'CLIENTS:':
+                $fields = explode(":", $rowa);
+                if ($fields[3] == 'ATC') {
+                    if (checkCountryIcao(substr($fields[0],0,2))) { 
+                        switch ($fields[16]) {
+                            case DateAdd(0):
+                            if (!isset($booking_controllers[0])) { $booking_controllers[0] = array(); }
+                            array_push($booking_controllers[0], $fields);
+                            break;
+                            
+                            case DateAdd(1):
+                            if (!isset($booking_controllers[1])) { $booking_controllers[1] = array(); }
+                            array_push($booking_controllers[1], $fields);
+                            break;
+                            
+                            case DateAdd(2):
+                            if (!isset($booking_controllers[2])) { $booking_controllers[2] = array(); }
+                            array_push($booking_controllers[2], $fields);
+                            break;
+                            
+/**/
+
+                            case DateAdd(3):
+                            if (!isset($booking_controllers[3])) { $booking_controllers[3] = array(); }
+                            array_push($booking_controllers[3], $fields);
+                            break;
+                            
+                            case DateAdd(4):
+                            if (!isset($booking_controllers[4])) { $booking_controllers[4] = array(); }
+                            array_push($booking_controllers[4], $fields);
+                            break;
+                            
+                            case DateAdd(5):
+                            if (!isset($booking_controllers[5])) { $booking_controllers[5] = array(); }
+                            array_push($booking_controllers[5], $fields);
+                            break;
+                            
+                            case DateAdd(6):
+                            if (!isset($booking_controllers[6])) { $booking_controllers[6] = array(); }
+                            array_push($booking_controllers[6], $fields);
+                            break;
+
+                        }
+                    }
+                } else {
+                    if (checkCountryIcao(substr($fields[11],0,2)) OR checkCountryIcao(substr($fields[13],0,2))) {
+                        array_push($booking_pilots, $fields);
+                    }
+                }
+                break;
+        }
+    }
+}
+
+function remove_accents( $string )
+{
+   $string = htmlentities($string);
+   return preg_replace("/&([a-z])[a-z]+;/i","$1",$string);
+}
+
+
+
+
+// BOOKINGS
+//-------------------------------------------------------------------------------------------------------------------
+// ATC
+
+
+
+
+function DateAdd($v,$d=null , $f="Ymd"){
+ 	
+$d=($d?$d:GMdate("Y-m-d")); 
+    return GMdate($f,strtotime($v." days",strtotime($d))); 
+}
+
+
+
+
+	// ATC
+//echo '<h3>' . $lng['atcingb'] . '</h3>';
+if (count($controllers) != 0) {
+    $capicao = '';
+  //  echo '<table width="100%">';
+    foreach ($controllers as $controller) {
+if (substr_count($controller[0],'_') == 1) {
+          list ($apicao, $position) = explode('_', $controller[0]);
+        } else {
+          list ($apicao, $position1, $position2) = explode('_', $controller[0]);
+          $position = $position2 . ' (' . $position1 .')';
+        }
+
+        if ($apicao != $capicao) {
+            $capicao = $apicao;
+          //  echo '<tr><td colspan="2"><strong>' . $apicao . ' - ' . $airports[$apicao] . '</td></tr>';
+        }
+        
+        // Realname
+        $realname = remove_accents(ucwords($controller[2]));
+        
+        // Level
+        $level = $ctrlevel[$controller[16]];
+        
+       // echo '<tr><td width="60"><a href="http://www.ivao.ca/flighttrack.php?cs='. $controller[0] .'" onClick="MM_openBrWindow(\'http://www.ivao.ca/flighttrack.php?cs='. $controller[0] .'\',\'\',\'scrollbars=yes,resizable=yes,width=700,height=600\');return false" target="_blank">&nbsp;&nbsp;' . $position . '</a></td><td><a href="http://www.ivao.aero/members/person/details.asp?id=' . $controller[1] . '" onClick="MM_openBrWindow(\'http://www.ivao.aero/members/person/details.asp?id=' . $controller[1] . '\',\'\',\'scrollbars=yes,resizable=yes,width=800,height=600\');return false" target="_blank" title="<b>' . $realname . '</b><br>">' . $controller[1] . '</a>&nbsp;(' . $level . ')</td></tr>';
+    }
+  //  echo '</table>';
+} else {
+
+   // echo '<p style="margin-bottom: 20px; margin-left: 10px; color: gray;"><i>' . $lng['noatcingb'] . '</i></p>';
+}
+
+
+
+
+$filecontentso = file_get_contents('http://www.ivao.aero/schedule/indd.asp');
+$rowso = split("\n", $filecontentso);
+$fileparto = '';
+$controllerso = array();
+
+//echo '<div class="bg"><table class="trafficlisttable">';
+foreach ($rowso as $rowo) {
+
+if (substr($rowso,0,2) == '!') {
+                $fileparto = substr($rowso,2);
+        }
+
+ 
+	$fieldsa = split(":", $rowo);
+	
+	$controllerso = $fieldsa[0];
+	
+	$rest = substr($controllerso, 0,2); 
+	
+	if ($rest == "SK") {
+		
+	$fechas = $fieldsa[6];	
+	$años = substr($fieldsa[6], 0,4); 
+	$mess = substr($fieldsa[6], 4, 2); 
+	
+
+	
+	$dias = substr($fieldsa[6], 6,2); 
+	
+	
+	//$hora = substr($fecha, 8,9); 
+	//$minutos = substr($fecha, 10,11); 
+	//$segundos = substr($fecha, 12,13); 
+	
+		if ($mes == 01) {
+		$meses = "Enero";
+	} else if ($mes == 02) {
+		$meses = "Febrero";
+	} else if ($mes == 03) {
+		$meses = "Marzo";
+	} else if ($mes == 04) {
+		$meses = "Abril";
+	} else if ($mes == 05) {
+		$meses = "Mayo";
+	} else if ($mes == 06) {
+		$meses = "Junio";
+	} else if ($mes == 07) {
+		$meses = "Julio";
+	} else if ($mes == 08) {
+		$meses = "Agosto";
+	} else if ($mes == 09) {
+		$meses = "Septiembre";
+	} else if ($mes == 10) {
+		$meses = "Octubre";
+	} else if ($mes == 11) {
+		$meses = "Noviembre";
+	} else if ($mes == 12) {
+		$meses = "Diciembre";
+	}
+	
+//echo '<tr><td><b><font color="black"><li>' . $fieldsa[0] . '</b>' . '     ' . $fieldsa[1] . '</li></font></td></tr>';
+//echo '<button class="btn btn-danger"><i class="fa fa-pencil"></i>Más Detalles</button>';
+
+
+} 
+}
+//echo '</table></div>';
+
+
+//echo '<button class="btn btn-danger"><i class="fa fa-pencil"></i>' .$lng['atcbingb'] . '</button>';
+
+
+
+// Pilots
+//echo '<h3>' . $lng['trafficingb'] . '</h3>';
+if (count($pilots) != 0) {
+   // echo '<table class="trafficlisttable">';
+    foreach ($pilots as $pilot) {
+        $realname = $pilot[2];
+        if (substr($realname,-5,1) == ' ') {
+            $realname = substr($realname,0,-5);
+        }
+        $realname = remove_accents(ucwords($pilot[2]));
+       // echo '<tr><td width="50"><a href="http://www.ivao.ca/flighttrack.php?cs=' . $pilot[0] . '" onClick="MM_openBrWindow(\'http://www.ivao.ca/flighttrack.php?cs=' . $pilot[0] . '\',\'\',\'scrollbars=yes,resizable=yes,width=700,height=600\');return false" target="_blank" title="' . $realname . '">' . $pilot[0] . '</a></td><td width="30">' . $pilot[11] . '</td><td>&gt;&nbsp;' . $pilot[13] . '</td></tr>';
+    }
+  //  echo '</table>';
+} else {
+  //  echo '<p style="margin-bottom: 20px; margin-left: 10px; color: gray;"><i>' .$lng['notrafficingb'] . '</i></p>';
+}
+?>
+
+
+<?
+
+//STAFF
+//-------------------------------------------------------------------------------------------------------------------
+if (count($staff) != 0) {
+  //  echo '<h3>' . $lng['staffingb'] . '</h3>
+  echo '<table class="trafficlisttable" width="160">';
+    foreach ($staff as $staffmember) {
+        $realname = $staffmember[2];
+        $realname = remove_accents(ucwords($realname));
+        echo '<tr><td><a href="http://www.ivao.aero/staff/details.asp?id=' . $staffmember[0] . '" target="_blank" onClick="MM_openBrWindow(\'http://www.ivao.aero/staff/details.asp?id=' . $staffmember[0] . '\',\'\',\'scrollbars=yes,resizable=yes,width=700,height=600\');return false" title="' . $staffmember[0] . '">'. $staffmember[0]. '</a></td></tr>';
+    }
+    echo '</table>';
+} else {
+	
+	echo "No hay Staff de División IVAO Colombia en Linea.";
+}
+
+?>
+
+
+
+<?
+
+echo '<br><hr>';
+echo 'There are '; echo count($controllers); echo' controllers, '; echo count($pilots); echo' pilots, and '; echo count($staff); echo' Staff members online in Colombia.';
+echo '<br><hr><br><h1>IVAO World</h1>';
+echo '<div class="trafficlistnettotal">' . sprintf($lng['totalonline'], $controllercount, $pilotcount) . '</div>';
+?>
                     </p>
                 </div>
              </div>
