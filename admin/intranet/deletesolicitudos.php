48f79362046ada2d30c8573b7010753f66d18ea8
Andres Esteban Zapata <Andres Esteban Zapata>
2016-07-03 16:58:31 -0500

    Procesos

---
+++ admin/intranet/deletesolicitudos.php
@@ -0,0 +1,37 @@
+
+
+
+
+
+<?php
+	
+		
+		$idss = $_GET['id'];
+			$web = $_GET['web'];
+		include('./db_login.php');
+		$db = new mysqli($db_host , $db_username , $db_password , $db_database);
+		if ($db->connect_errno > 0) {
+			die('Unable to connect to database [' . $db->connect_error . ']');
+		}
+		
+		
+		
+		
+			$sql1 = "delete from solicitudeseventosatc where id='$id'";  
+
+		if (!$result1 = $db->query($sql1)) {
+			die('There was an error running the query [' . $db->error . ']');
+		}
+		
+		
+		
+		
+	
+?>
+
+
+<script>
+alert('Información satisfactoria.');
+window.location = './?page=infoeventoatcmas&id=<?php echo $web; ?>';
+</script>
+
