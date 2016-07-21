1dc5fad23d8c94f0b9c014d09675ed366b5ac738
Andres Esteban Zapata <Andres Esteban Zapata>
2016-07-21 13:43:36 -0500

    Fix

--- usuarios/tours.php
+++ usuarios/tours.php
@@ -1,5 +1,5 @@
 
 <div style="width:100%;height:530px; position:relative; overflow:hidden">
-<iframe src="http://tours.ivao.aero/cgi-bin/tours.pl?session=ToHe06Rc1tzF&division=CO&function=Screen&width=640&height=480" style="top:-27px; position:absolute" width="100%" height="650px"  frameborder="0" >
+<iframe src="http://tours.ivao.aero/cgi-bin/tours.pl?division=CO&width=640&height=480&IVAOTOKEN=<?php echo $_COOKIE[cookie_name]; ?>" style="top:-27px; position:absolute" width="100%" height="650px"  frameborder="0" >
 </iframe>
 </div>
